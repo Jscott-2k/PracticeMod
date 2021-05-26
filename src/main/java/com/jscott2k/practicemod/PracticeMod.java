8b219a94c3f619a371d620268c91b431ea2fe34b
Justin Scott <Jstman2000@gmail.com>
2021-05-25 22:02:12 -0400

    Sun Mask
    Added sun mask item and Initializer class to handle degered registrations.

--- src/main/java/com/jscott2k/practicemod/PracticeMod.java
+++ src/main/java/com/jscott2k/practicemod/PracticeMod.java
@@ -1,9 +1,14 @@
 package com.jscott2k.practicemod;
 
+import com.jscott2k.practicemod.core.init.Initializer;
+import com.jscott2k.practicemod.core.init.ItemInitializer;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -22,28 +27,36 @@ import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("practicemod")
-public class PracticeMod
-{
+@Mod.EventBusSubscriber(modid = PracticeMod.MODID)
+public class PracticeMod {
+    public static final String MODID = "practicemod";
+
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     public PracticeMod() {
+
+        Initializer.init();
+        ItemInitializer.init();
+
+        IEventBus modeEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        modeEventBus.addListener(this::setup);
         // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        modeEventBus.addListener(this::enqueueIMC);
         // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        modeEventBus.addListener(this::processIMC);
         // Register the doClientStuff method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        modeEventBus.addListener(this::doClientStuff);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
+    private void setup(final FMLCommonSetupEvent event) {
         // some preinit code
+
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
@@ -53,19 +66,21 @@ public class PracticeMod
         LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
     }
 
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
+    private void enqueueIMC(final InterModEnqueueEvent event) {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo("examplemod", "helloworld", () -> {
+            LOGGER.info("Hello world from the MDK");
+            return "Hello world";
+        });
     }
 
-    private void processIMC(final InterModProcessEvent event)
-    {
+    private void processIMC(final InterModProcessEvent event) {
         // some example code to receive and process InterModComms from other mods
         LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m->m.getMessageSupplier().get()).
+                map(m -> m.getMessageSupplier().get()).
                 collect(Collectors.toList()));
     }
+
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
@@ -75,7 +90,7 @@ public class PracticeMod
 
     // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
     // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class RegistryEvents {
         @SubscribeEvent
         public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
--- src/main/java/com/jscott2k/practicemod/core/init/Initializer.java
+++ src/main/java/com/jscott2k/practicemod/core/init/Initializer.java
@@ -2,16 +2,15 @@ package com.jscott2k.practicemod.core.init;
 
 import com.jscott2k.practicemod.PracticeMod;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-public class ItemInitializer  {
-
-
-    private final static RegistryObject<Item> SUN_MASK = Initializer.ITEMS.register("sun_mask", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
-    public static void init(){}
+public class Initializer {
+    public final static DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, PracticeMod.MODID);
+    public static void init(){
+        IEventBus modeEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ITEMS.register(modeEventBus);
+    }
 }
